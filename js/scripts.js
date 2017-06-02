9bd8a12821137c4748858320cd9ed39957ce0d70
Katarzyna Lachcik <kaisek@poczta.fm>
2017-06-02 15:49:05 +0100

    add else if and else

--- js/scripts.js
+++ js/scripts.js
@@ -4,12 +4,21 @@ var a = prompt('a='),
 alert('Wynik działania (a * a) + (2 * a * b) - (b * b) = ' + value);
 console.log('Wynik działania (a * a) + (2 * a * b) - (b * b) = ' + value);
 
-if (value > 0){
-	console.log('wynik dodatni');
-}
-if (value < 0){
+if (value < 0) {
 	console.log('wynik ujemny');
 }
-if (value == 0){
+else if (value > 0) {
+	console.log('wynik dodatni');
+}
+else {
 	console.log('wynik równy zero');
 }
+// if (value > 0){
+// 	console.log('wynik dodatni');
+// }
+// if (value < 0){
+// 	console.log('wynik ujemny');
+// }
+// if (value == 0){
+// 	console.log('wynik równy zero');
+// }
